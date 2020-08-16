d7938ab191a721e71f6adf368725b2685c76499d
fohlle <patte2000@hotmail.com>
2020-08-16 18:55:32 +0200

    display

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+
+const hours = document.querySelector(".hours");
+const minutes = document.querySelector(".minutes");
+const seconds = document.querySelector(".seconds");
+const pm = document.querySelector(".pm");
+
+
+function update(){
+    
+}
