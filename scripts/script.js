af9e20495b6b75aba63aa5020cc1be4211d532a0
fohlle <patte2000@hotmail.com>
2020-08-17 19:20:12 +0200

    working clock

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,22 @@
 const hours = document.querySelector(".hours");
 const minutes = document.querySelector(".minutes");
 const seconds = document.querySelector(".seconds");
-const pm = document.querySelector(".pm");
+
+const alarmBtn = document.querySelector(".set-alarm");
+const alarmText = document.querySelector(".alarm");
+
 
 
 function update(){
-    
+    hours.innerText = new Date().getHours();
+    minutes.innerText = new Date().getMinutes();
+    seconds.innerText = new Date().getSeconds();
+    console.log("hi");
 }
+
+let time = setInterval(update,1000);
+
+alarmBtn.addEventListener("click", () => {
+    
+})
+//update();
